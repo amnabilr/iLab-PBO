52c84b7830ae1a0e8a9485c76d54aec9b15b7169
Andi Mohammad Nabil Ramadan <92446784+amnabilr@users.noreply.github.com>
2022-07-11 20:22:33 +0700

    Create Lingkaran.java

---
+++ Lingkaran.java
@@ -0,0 +1,10 @@
+public class Lingkaran extends Bangun{
+    private double radius;
+    public void setRadius(double radius){
+        this.radius = radius;
+        setLuas(38.48);
+    }
+    public double getRadius(){
+        return radius;
+    }
+}
